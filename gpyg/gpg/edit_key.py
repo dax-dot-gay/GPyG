e7178f07db0331a0ca65f8ee56d677ca26031750
Dax Harris <matteovh@gmail.com>
2024-03-12 15:36:59 -0400

    preference modification

--- gpyg/gpg/edit_key.py
+++ gpyg/gpg/edit_key.py
@@ -1,9 +1,39 @@
 import datetime
 from enum import StrEnum
 import re
+from typing import List
 from pydantic import BaseModel
 from .models import KeyInfo
 from ..util import SubprocessSession, SubprocessResult
+
+
+class PrefCiphers(StrEnum):
+    PLAINTEXT = "S0"
+    IDEA = "S1"
+    TRIPLE_DES = "S2"
+    CAST5 = "S3"
+    BLOWFISH = "S4"
+    AES_128 = "S7"
+    AES_192 = "S8"
+    AES_256 = "S9"
+    TWOFISH = "S10"
+
+
+class PrefsDigest(StrEnum):
+    MD5 = "H1"
+    SHA_1 = "H2"
+    RIPE_MD = "H3"
+    SHA_256 = "H8"
+    SHA_384 = "H9"
+    SHA_512 = "H10"
+    SHA_224 = "H11"
+
+
+class PrefsCompression(StrEnum):
+    UNCOMPRESSED = "Z0"
+    ZIP = "Z1"
+    ZLIB = "Z2"
+    BZIP2 = "Z3"
 
 
 class KeyListItem(BaseModel):
@@ -140,21 +170,62 @@ class KeyEditor:
         keys = [KeyListItem.from_string(seg[1]) for seg in segments if seg[0] == "KEY"]
         users = [UIDListItem.from_string(seg[1]) for seg in segments if seg[0] == "UID"]
         return keys, users
-    
+
     def select_key(self, id: str | None) -> None:
-        self.execute(f"key {id if id else "0"}")
-        
-    def select_user_id(self, index: int | str | None) -> None:
-        self.execute(f"uid {index if index else "0"}")
-        
+        self.execute(f"key {id if id else '0'}")
+
+    def select_user_id(self, user_index: int | str | None) -> None:
+        self.execute(f"uid {user_index if user_index else '0'}")
+
     def add_user_id(self, name: str, email: str | None = None, comment: str | None = None) -> None:
         self.execute("adduid", name, email, comment)
-        
-    def delete_user_id(self, index: int) -> None:
-        self.select_user_id(index)
+
+    def delete_user_id(self, user_index: int) -> None:
+        self.select_user_id(user_index)
         self.execute("deluid", "y")
-        
-    def revoke_user_id(self, index: int, reason: RevocationEnum = RevocationEnum.INVALID_UID, description: str | None = None) -> None:
-        self.select_user_id(index)
+        self.select_user_id(None)
+
+    def revoke_user_id(
+        self,
+        user_index: int,
+        reason: RevocationEnum = RevocationEnum.INVALID_UID,
+        description: str | None = None,
+    ) -> None:
+        self.select_user_id(user_index)
         self.execute("revuid", "y", reason.value, description, "", "y")
-        
+        self.select_user_id(None)
+
+    def set_preferences(
+        self,
+        user_index: int | str | None,
+        cipher: List[PrefCiphers | str] = [PrefCiphers.TRIPLE_DES],
+        digest: List[PrefsDigest | str] = [PrefsDigest.SHA_1],
+        compression: List[PrefsCompression | str] = [PrefsCompression.UNCOMPRESSED],
+        extras: List[str] = [],
+    ):
+        pref_string = " ".join(
+            [
+                i
+                for i in [
+                    " ".join(cipher) if len(cipher) > 0 else None,
+                    " ".join(digest) if len(digest) > 0 else None,
+                    " ".join(compression) if len(compression) > 0 else None,
+                    " ".join(extras) if len(extras) > 0 else None,
+                ]
+                if i != None
+            ]
+        )
+        self.select_user_id(user_index)
+        print(self.execute(f"setpref {pref_string}", "y"))
+
+    def get_preferences(self, user_index: int) -> dict[str, List[str]]:
+        self.select_user_id(user_index)
+        result = {
+            i.strip()
+            .split(":")[0]
+            .lower(): [x.strip() for x in i.strip().split(":")[1].split(",")]
+            for i in self.execute("showpref").split("\n")
+            if not i.startswith("[") and len(i.strip()) > 0
+        }
+        self.select_user_id(None)
+        return result
