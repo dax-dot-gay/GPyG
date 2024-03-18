d605f63a2e82d6615adc3c513207493c03cf6118
Dax Harris <dharr@lle.rochester.edu>
2024-03-18 12:29:52 -0400

    add tests for current key ops

--- conftest.py
+++ conftest.py
@@ -14,6 +14,33 @@ def homed_context(homedir: str):
         yield gpg
 
 
+@pytest.fixture(scope="module")
+def session(tmp_path_factory: pytest.TempPathFactory):
+    with gpyg.GPG(homedir=str(tmp_path_factory.mktemp("homedir").resolve())) as gpg:
+        gpg.keys.generate_key(
+            "test-name",
+            email="test-email",
+            key_type="RSA",
+            key_length=3072,
+            passphrase="test-psk",
+        )
+        gpg.keys.generate_key(
+            "test-name2",
+            email="test-email2",
+            key_type="DSA",
+            key_length=2048,
+            passphrase="test-psk",
+        )
+        gpg.keys.generate_key(
+            "test-name3",
+            email="test-email3",
+            key_type="RSA",
+            key_length=2048,
+            passphrase="test-psk",
+        )
+        yield gpg
+
+
 @pytest.fixture
 def homedir(tmp_path_factory: pytest.TempPathFactory) -> str:
     return str(tmp_path_factory.mktemp("homedir").resolve())
--- tests/test_key.py
+++ tests/test_key.py
@@ -1,1 +1,35 @@
 from gpyg import GPG
+
+
+def test_generate(homed_context):
+    result = homed_context.keys.generate_key(
+        "test-name",
+        email="test-email",
+        key_type="RSA",
+        key_length=3072,
+        passphrase="test-psk",
+    )
+    assert len(result.uids) == 1
+    uid = result.uids[0]
+    assert uid.name == "test-name"
+    assert uid.email == "test-email"
+    assert result.fingerprint
+
+
+def test_list(session):
+    keys = session.keys.list_keys()
+    assert len(keys) == 3
+    assert all([len(i.uids) == 1 for i in keys])
+
+    keys = session.keys.list_keys(secret=True)
+    assert len(keys) == 3
+    assert all([len(i.uids) == 1 for i in keys])
+
+
+def test_get(session):
+    keys = session.keys.list_keys()
+    assert len(keys) == 3
+
+    result = session.keys.get_key(keys[0].fingerprint)
+    assert result.fingerprint == keys[0].fingerprint
+    assert result == keys[0]
