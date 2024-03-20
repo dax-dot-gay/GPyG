8decf7a5d522d4642dc758fb57126412b3d1f4d5
Dax Harris <dharr@lle.rochester.edu>
2024-03-20 11:11:53 -0400

    test setup

---
+++ conftest.py
@@ -0,0 +1,22 @@
+import pytest
+from gpyg import GPG
+
+
+@pytest.fixture(scope="module")
+def scoped_homedir(tmp_path_factory: pytest.TempPathFactory) -> str:
+    return str(tmp_path_factory.mktemp("gpg-homedir").absolute())
+
+
+@pytest.fixture
+def homedir(tmp_path_factory: pytest.TempPathFactory) -> str:
+    return str(tmp_path_factory.mktemp("gpg-homedir").absolute())
+
+
+@pytest.fixture(scope="module")
+def scoped_instance(scoped_homedir) -> GPG:
+    return GPG(homedir=scoped_homedir, kill_existing_agent=True)
+
+
+@pytest.fixture
+def instance(homedir) -> GPG:
+    return GPG(homedir=scoped_homedir, kill_existing_agent=True)
